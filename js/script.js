45cac47f908f4e9dbad474ff692a191e0ce2dd56
SekretarevaNastya <61866591+SekretarevaNastya@users.noreply.github.com>
2020-04-30 17:53:37 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,14 @@
-let tab = function () {
-    let tabNav = document.querySelectorAll('.tabs-nav'),
-        heroContent = document.querySelectorAll('.hero'),
-        heroName;
+let logo = document.querySelector('.logo');
+let hero = document.querySelector('.hero');
+let slider = document.querySelector('.slider');
+let Back = document.querySelector('.Back');
+let logo1 = document.querySelector('.logo1');
+let line = document.querySelector('.line');
 
-    tabNav.forEach(item => {
-        item.addEventListener('click', selectTabNav)
-    });
-
-    function selectTabNav() {
-		tabNav.forEach(item => {
-            item.classList.remove('is-active');
-        });
-		
-		 this.classList.add('is-active');
-        heroName = this.getAttribute('data-hero-name');
-        
-        
-			selectNeroContent(heroName);
-			
-           
-    }
-
-    function selectNeroContent(heroName) {
-        heroContent.forEach(item => {
-            item.classList.contains(heroName) ? item.classList.add('is-active') : item.classList.remove('is-active');
-        })
-    }
-
+hero.onclick = function () {
+	logo.style.display= "none";
+	slider.style.display= "block";
+	Back.style.display= "block";
+	logo1.style.display= "block";
+	line.style.display= "block";
 };
-
-
-tab();
-
-
-
-
-
-
